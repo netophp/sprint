ef12d6b0cd2eb1713aed687e41a11850fabd1147
Julio Ramirez <rrodriguez.julio@gmail.com>
2015-09-17 17:11:20 -0500

    coding #5 se declara interface AbstracGenericDao

---
+++ api/src.main.java/spring/test/mx/dao/AbstractGenericDao.java
@@ -0,0 +1,15 @@
+package spring.test.mx.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface AbstractGenericDao<T, PK extends Serializable> {
+
+    void create(T newInstance);
+    T read(PK id);
+    void update(T transientObject);
+    void delete(T persistentObject);
+    List<T> findAll();
+    long count();
+
+}
