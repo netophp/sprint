2c188bf79a7565308e79cb3ee5c9df27d106dfae
Julio Ramirez <rrodriguez.julio@gmail.com>
2016-02-09 20:19:41 -0600

    coding #8 se modifica import por modificacion de group id

---
+++ core/src/main/java/netophp/mx/dao/impl/AbstractGenericDaoHibernateImpl.java
@@ -0,0 +1,56 @@
+package netophp.mx.dao.impl;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import spring.test.mx.dao.AbstractGenericDao;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+public abstract class AbstractGenericDaoHibernateImpl<T, PK extends Serializable> implements AbstractGenericDao<T, PK> {
+
+    private SessionFactory sessionFactory;
+
+    public AbstractGenericDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void create(T newInstance) {
+        currentSession().save(newInstance);
+    }
+
+    @SuppressWarnings("unchecked")
+    public T read(PK id) {
+        return (T) currentSession().get(getType(), id);
+    }
+
+    public void update(T transientObject) {
+        currentSession().update(transientObject);
+    }
+
+    public void delete(T persistentObject) {
+        currentSession().delete(persistentObject);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> findAll() {
+        return currentSession().createQuery("from " + getType().getName()).list();
+    }
+
+    public long count() {
+        Query query = currentSession().createQuery("Select count(*) from " + getType().getName());
+        return (Long) query.uniqueResult();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private Class<?> getType() {
+        ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
+        return (Class) parameterizedType.getActualTypeArguments()[0];
+    }
+
+    public Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+}
