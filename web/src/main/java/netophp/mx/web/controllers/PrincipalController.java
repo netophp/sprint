18b82b08a4a397503764eeaefa3e5bc5336734d3
Julio Ramirez <rrodriguez.julio@gmail.com>
2016-02-09 20:36:58 -0600

    coding #9 se inyecta servicio HelloWorldzService

--- web/src/main/java/netophp/mx/web/controllers/PrincipalController.java
+++ web/src/main/java/netophp/mx/web/controllers/PrincipalController.java
@@ -2,6 +2,8 @@ package netophp.mx.web.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import netophp.mx.service.HelloWorldzService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class PrincipalController {
-	
+
+	@Autowired
+	HelloWorldzService helloWorldzService;
+
 	@RequestMapping("/")
 	public String principal(Model modelo, HttpSession sesion) {
+		helloWorldzService.getHelloWorldz();
 		return "principal/index";
 	}
 }
