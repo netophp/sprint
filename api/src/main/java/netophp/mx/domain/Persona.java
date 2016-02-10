53bd132c5e3622cee3f56626f1b3f9887d5b94f6
Julio Ramirez <rrodriguez.julio@gmail.com>
2016-02-10 10:34:16 -0600

    coding #11 se agrega propiedad id para persona

--- api/src/main/java/netophp/mx/domain/Persona.java
+++ api/src/main/java/netophp/mx/domain/Persona.java
@@ -2,10 +2,19 @@ package netophp.mx.domain;
 
 public class Persona {
 
+    private Long id;
     private String nombre;
     private String apellido;
     private String email;
     private Integer edad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNombre() {
         return nombre;
