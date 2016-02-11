ce49b234e6954173c458b5df8ed8fdeaefc17527
Julio Ramirez <rrodriguez.julio@gmail.com>
2016-02-11 11:24:17 -0600

    coding #19 se ajusta esquema de creacion para la sequence

--- core/src/main/resources/SQL/ddl.sql
+++ core/src/main/resources/SQL/ddl.sql
@@ -1,10 +1,11 @@
 
 CREATE TABLE DIVER.PERSONA (
+         id         NUMBER,
          nombre     VARCHAR2(100),
          apellido   VARCHAR2(150),
          email      VARCHAR2(100),
          edad       NUMBER);
 COMMIT;
 
-CREATE SEQUENCE sq_id_persona INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE DIVER.sq_id_persona INCREMENT BY 1 START WITH 1;
 COMMIT;
