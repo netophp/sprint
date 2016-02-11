cf50eb63c5117273091183bb899858a13466d3f0
Julio Ramirez <rrodriguez.julio@gmail.com>
2016-02-11 11:42:44 -0600

    coding #19 se implementa prueba de integracion CRUD

---
+++ core/src/test/java/netophp.mx/integracion/PersonaDaoIntegrationTest.java
@@ -0,0 +1,69 @@
+package netophp.mx.integracion;
+
+import netophp.mx.dao.PersonaDao;
+import netophp.mx.domain.Persona;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/DataSourceAppCtx.xml", "/PropertiesAppCtx.xml", "/HibernateAppCtx.xml", "/AopAppCtx.xml"})
+@Transactional
+public class PersonaDaoIntegrationTest {
+
+    @Autowired
+    PersonaDao personaDao;
+
+    Persona persona = new Persona();
+
+    static Long personaId = 0L;
+
+    @Test
+    @Rollback(false)
+    public void test001creaPersona() {
+        persona.setNombre("Neto");
+        persona.setApellido("Davila");
+        persona.setEmail("netophp@gmail.com");
+        persona.setEdad(40);
+        personaDao.create(persona);
+        assertTrue(persona.getId() > 0);
+        personaId = persona.getId();
+    }
+
+    @Test
+    public void test002readPersona() {
+        Persona readPersona = personaDao.read(personaId);
+        assertNotNull(readPersona.getNombre());
+    }
+
+    @Test
+    public void test003updatePersona() {
+        Persona updatePersona = personaDao.read(personaId);
+        updatePersona.setNombre("Netillo");
+        personaDao.update(updatePersona);
+        Persona updatedPersona = personaDao.read(personaId);
+        assertTrue(updatedPersona.getNombre().equals("Netillo"));
+    }
+
+    @Test
+    @Rollback(false)
+    public void test004updatePersona() {
+        personaDao.delete(persona);
+    }
+
+}
